6edf8598d6792d21389136d99607ee249e40be49
srpineda <srpineda@up.edu.ph>
2020-09-10 14:34:14 +0800

    Updates

---
+++ precache-manifest.3a9000e8d930448f321cfa374719212c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321acffddc6af4e987f1ca966377626c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "31f4541c7512c31cc192",
+    "url": "/robofriends/static/css/main.df90a9ac.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f4541c7512c31cc192",
+    "url": "/robofriends/static/js/main.18cee909.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
